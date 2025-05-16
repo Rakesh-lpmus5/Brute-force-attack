687a31bee82eda71b4b22b65b4c5642dbb6f62e9
Hulk4998 <sankhlar993@gmail.com>
2025-05-16 07:51:17 +0530

    update

--- direbasebfa.py
+++ direbasebfa.py
@@ -4,15 +4,14 @@ from tqdm import tqdm
 def brute_force_zip(zip_file_path, wordlist_path):
     zip_file = zipfile.ZipFile(zip_file_path)
 
-    with open(wordlist_path, 'r', encoding='utf-8', errors='ignore') as wordlist:
-        passwords = [line.strip() for line in wordlist if line.strip()]  # remove empty lines
+    with open(wordlist_path, 'r') as wordlist:
+        passwords = wordlist.readlines()
 
-    print(f"[i] Trying {len(passwords)} passwords...")
-
-    for password in tqdm(passwords, desc="Brute-forcing", unit="pw"):
+    for password in tqdm(passwords, desc="Trying passwords"):
+        password = password.strip()
         try:
             zip_file.extractall(pwd=password.encode('utf-8'))
-            print(f"\n[+] Password found: {password}")
+            print(f"[+] Password found: {password}")
             return
         except:
             continue
